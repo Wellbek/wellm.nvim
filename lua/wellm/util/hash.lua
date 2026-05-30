541cf61514dc80923b9056a3e7605bd39a2078f5
Wellbek <louis.wellmeyer@gmail.com>
2026-05-30 12:36:23 +0900

    fix(hash): Replace Lua 5.3 bitwise ~ operator with bit.bxor/bit.band for LuaJIT

--- lua/wellm/util/hash.lua
+++ lua/wellm/util/hash.lua
@@ -3,8 +3,8 @@ local M = {}
 local function fnv1a(str)
   local hash = 2166136261
   for i = 1, #str do
-    hash = hash ~ string.byte(str, i)
-    hash = (hash * 16777619) & 0xFFFFFFFF
+    hash = bit.bxor(hash, string.byte(str, i))
+    hash = bit.band(hash * 16777619, 0xFFFFFFFF)
   end
   return string.format("%08x", hash)
 end
